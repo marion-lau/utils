b90a3ce8bc86d2373edf125db32a0d99da4bf567
marion.lau <marion.lau.z@gmail.com>
2016-06-03 22:51:02 +0800

    create utils base

---
+++ utils.js
@@ -0,0 +1,23 @@
+;
+/*!
+ * Marion JavaScript Library v1.0.1
+ * https://github.com/marion-lau/
+ * 
+ * Copyright Marion Studio and other contributors
+ *
+ * Date: 2016-06-01
+ */
+! function(window, factory) {
+	if (typeof module === 'object' && typeof module.exports === 'object') {
+		module.exports = global.document ?
+			factory(global, true) :
+			function(w) {
+				if (!w.document) {
+					throw new Error('the Utils requires a window with a document');
+				}
+				return factory(w);
+			};
+	} else {
+		factory(global);
+	}
+}(typeof window !== 'undefined' ? window : this, function(window, noGlobal) {});
